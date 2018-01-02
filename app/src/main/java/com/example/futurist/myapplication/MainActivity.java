65fff5516377c578491a92746ccd4e13a25c4df3
Sharabaddin <general1308@gmail.com>
2018-01-02 18:03:27 +0200

    add screen #3 "rss item page"

--- app/src/main/java/com/example/futurist/myapplication/MainActivity.java
+++ app/src/main/java/com/example/futurist/myapplication/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.futurist.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
+
+import java.nio.channels.ScatteringByteChannel;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +13,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        setContentView(R.layout.add);
+        setContentView(R.layout.item);
+
+        // For scrolling text in item.xml layout
+        //TextView textView = (TextView) findViewById(R.id.textViewText);
+        //textView.setMovementMethod(new ScrollingMovementMethod());
     }
 }
